09c7e5997ab3d2a1dd090c2502ad070f77f8c901
R. Massimo <massimo.rizzi@nextre.it>
2019-11-07 14:55:49 +0100

    Primo commit della repository spring

--- src/main/java/it/nextre/academy/nxtlearn/config/appConfig.java
+++ src/main/java/it/nextre/academy/nxtlearn/config/appConfig.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class appConfig {
     @Bean
-    public Logger getLogger(){
+    public Logger getLogger() {
         return LoggerFactory.getLogger(this.getClass());
     }
 }//end class
